9936e9697da37c143d8ad491721d025b8c393dbe
Shiyuu <241880474@smail.nju.edu.cn>
2026-02-14 18:13:14 +0800

    日常

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Shiyuu-official.github.io',
-  base:'/', 
-	integrations: [mdx(), sitemap()],
+  base: '/',
+  markdown: {
+    remarkPlugins: [remarkGfm],
+  },
+	integrations: [
+    mdx({
+      extendMarkdownConfig: true,
+    }),
+    sitemap(),
+  ],
 });
