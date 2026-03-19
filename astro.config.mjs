4b41c7b1002df57ba08553966d83c5f00601a5dc
Shiyuu <241880474@smail.nju.edu.cn>
2026-03-19 21:09:47 +0800

    日常

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Shiyuu-official.github.io',
   base: '/',
   markdown: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 	integrations: [
     mdx({
